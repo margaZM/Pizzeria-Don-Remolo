23c32f21b2eb73eb12fb12dfb077f794e4cd0663
margaZM <aurazambrano227@gmail.com>
2022-07-10 14:21:26 -0500

    fix image not loaded

--- modules/home/components/Categories/Category.js
+++ modules/home/components/Categories/Category.js
@@ -1,21 +1,23 @@
 import Image from 'next/image';
 import React from 'react';
 
-export const Category = ({ image, title, index }) => {
-	console.log(index);
+export const Category = ({ image, title }) => {
+	console.log(image);
 	return (
 		<section
 			className={
 				'flex flex-col items-center p-4 relative hover:scale-125 hover:cursor-pointer'
 			}
 		>
-			<Image
-				loader={() => image}
-				src={image}
-				alt="category-food-image"
-				width="400"
-				height="260"
-			/>
+			{image && (
+				<Image
+					loader={() => image}
+					src={image}
+					alt="category-food-image"
+					width="400"
+					height="260"
+				/>
+			)}
 			<p className="mt-4 font-bold"> {title} </p>
 		</section>
 	);
