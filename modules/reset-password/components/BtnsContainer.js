60f572cca351399a4f840188c5692d21000cbf2f
Fede <f.prog@outlook.com>
2022-07-15 22:00:15 -0300

    reset-password: cancel button now redirects to home page

--- modules/reset-password/components/BtnsContainer.js
+++ modules/reset-password/components/BtnsContainer.js
@@ -1,17 +1,17 @@
-import { useOnModalChange } from "../../../hooks/useOnModalChange";
+import { useRouter } from "next/router";
 import { registerButtonStyle } from "../../home-header/styles/buttons_styles";
 import { Button } from "./Button";
 
 export const BtnsContainer = ({ btn_2_text, step_3 }) => {
-	const { handleWindow } = useOnModalChange();
+	const router = useRouter();
+	const handleRedirect = () => router.push("/");
 	return (
 		<div className="btns-container flex justify-end items-end w-full h-full p-3">
 			<div className="flex gap-3">
 				<Button 
 					style='max-w-[100px] min-h-[40px] text-[black] font-bold bg-transparent-bg border-none active:bg-white' 
 					text='Cancelar'
-					data-modal='forgot-password'
-					onClick={handleWindow}
+					onClick={handleRedirect}
 				/>
 				<Button 
 					type='submit' 
