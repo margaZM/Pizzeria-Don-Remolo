3e8f511785f3cba0d2661b5299ca65e702997942
Fede <f.prog@outlook.com>
2022-07-18 19:40:38 -0300

    useRedirect modified for it can accept both target params and normal params

--- hooks/useRedirect.js
+++ hooks/useRedirect.js
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router';
 export const useRedirect = () => {
 	const router = useRouter();
 	const handleRedirect = (e) => {
-		let path = e.target.dataset.path;
+		let path = e.target ? e.target.dataset.path : e;
 		router.push(path);
 	};
 	return { handleRedirect };
