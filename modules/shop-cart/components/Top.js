49843027430e1a4df53dc202cda1c383b5e3d1c1
Fede <f.prog@outlook.com>
2022-07-22 19:17:00 -0300

    shop-cart modal created

---
+++ modules/shop-cart/components/Top.js
@@ -0,0 +1,14 @@
+import { Cart } from "../../shared/Cart";
+import { CloseBtn } from "../../shared/CloseBtn";
+
+export const Top = () => {
+	return (
+		<article className="flex justify-between max-h-[50px]">
+			<Cart />
+			<p className="flex justify-center items-center text-[1.4rem] font-bold">Tu carrito</p>
+			<div className="flex justify-center items-center max-h-[45px] pb-1">
+				<CloseBtn modal="cart" />
+			</div>
+		</article>
+	);
+};
