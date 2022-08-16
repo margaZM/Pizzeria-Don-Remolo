ca9856256b288a35621e6342456423318fe0bc27
Fede <f.prog@outlook.com>
2022-08-16 19:13:14 -0300

    Most popular section update
    -Before this fix, it wasn't showing the product info.
    -Product services has been modify.
    -Product requests file has been deleted.

--- services/product-services/productServices.js
+++ services/product-services/productServices.js
@@ -1,10 +1,53 @@
-import REQUESTS from './productsRequests';
+import axios from "axios";
+import { URL } from "../baseService";
+
+const ALL_PRODUCTS = `${URL}/api/products`;
+const PROMOTIONS = `${URL}/api/promotions`;
+const INGREDIENTS = `${URL}/api/ingredients`;
+const MOST_POPULAR = `${URL}/api/products/mostpopular`;
+
+const getAllProducts = async (params) => {
+	const res = await axios.get(`${ALL_PRODUCTS}?PageSize=${params?.pageSize || 6}&PageIndex=${params?.pageIndex || 1}`);
+	return res;
+};
+
+const getProductsByCategory = async (params) => {
+	const res = await axios.get(`${ALL_PRODUCTS}?PageSize=${params?.pageSize || 6}&PageIndex=${params?.pageIndex || 1}&Category=${params?.category || 'pizzas'}`);
+	return res;
+};
+
+const getPromotions = async () => {
+	const res = axios.get(PROMOTIONS);
+	return res;
+};
+
+const getIngredients = async (id) => {
+	const res = axios.get(!id ? INGREDIENTS : `${INGREDIENTS}/${id}`);
+	return res;
+};
+const getMostPopular = async (id) => {
+	const res = axios.get(MOST_POPULAR);
+	return res;
+};
+
+const searchProduct = async (product) => {
+	const res = axios.get(`${ALL_PRODUCTS}?Search=${product}`);
+	return res;
+};
+
+const searchPromotionById = async (e) => {
+	const id = e.target ? e.target.dataset.id : e;
+	const res = await axios.get(PROMOTIONS);
+	const selectedProduct = res.data.filter(prod => +id === +prod.id);
+	return selectedProduct;
+};
 
 export const productServices = {
-	getAllProducts: REQUESTS.getAllProducts,
-	getProductsByCategory: REQUESTS.getProductsByCategory,
-	getPromotions: REQUESTS.getPromotions,
-	getIngredients: REQUESTS.getIngredients,
-	searchProduct: REQUESTS.searchProduct,
-	searchPromotionById: REQUESTS.searchPromotionById,
+	getAllProducts,
+	getProductsByCategory,
+	getPromotions,
+	getIngredients,
+	getMostPopular,
+	searchProduct,
+	searchPromotionById
 };
